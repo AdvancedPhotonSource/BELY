ca9281e00453cb0876cf02c004bc32341592478f
Dariusz Jarosz <djarosz@anl.gov>
2023-12-15 09:01:05 -0600

    Add basic create wizard controller.

---
+++ src/java/CdbWebPortal/src/java/gov/anl/aps/cdb/portal/controllers/extensions/ItemCreateWizardDomainLogbookController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) UChicago Argonne, LLC. All rights reserved.
+ * See LICENSE file.
+ */
+package gov.anl.aps.cdb.portal.controllers.extensions;
+
+import gov.anl.aps.cdb.portal.controllers.ItemController;
+import gov.anl.aps.cdb.portal.controllers.ItemDomainLogbookController;
+import gov.anl.aps.cdb.portal.utilities.SessionUtility;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import org.primefaces.event.FlowEvent;
+
+/**
+ *
+ * @author djarosz
+ */
+@Named(ItemCreateWizardDomainLogbookController.controllerNamed)
+@SessionScoped
+public class ItemCreateWizardDomainLogbookController extends ItemCreateWizardController implements Serializable {
+
+    public final static String controllerNamed = "itemCreateWizardDomainLogbookController";   
+
+    @Override
+    public String getItemCreateWizardControllerNamed() {
+        return controllerNamed; 
+    }    
+    
+    ItemDomainLogbookController itemDomainController = null; 
+    @Override
+    public ItemController getItemController() {
+        if (itemDomainController == null) {
+            itemDomainController = ItemDomainLogbookController.getInstance(); 
+        }
+        return itemDomainController;         
+    }
+    
+    public static ItemCreateWizardDomainLogbookController getInstance() {
+        return (ItemCreateWizardDomainLogbookController) SessionUtility.findBean(controllerNamed);
+    }
+    
+    @Override
+    public String getFirstCreateWizardStep() {
+        return ItemCreateWizardSteps.basicInformation.getValue();
+    }            
+    
+    
+}
