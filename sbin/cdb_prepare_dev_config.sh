2d2aad8c8eb98175de0bc412173bb801a8824c99
Dariusz Jarosz <djarosz@anl.gov>
2023-12-19 13:27:36 -0600

    Resolve development configuraiton. Closes #25

--- sbin/cdb_prepare_dev_config.sh
+++ sbin/cdb_prepare_dev_config.sh
@@ -13,73 +13,73 @@
 #
 
 MY_DIR=`dirname $0` && cd $MY_DIR && MY_DIR=`pwd`
-if [ -z "${CDB_ROOT_DIR}" ]; then
-    CDB_ROOT_DIR=$MY_DIR/..
+if [ -z "${LOGR_ROOT_DIR}" ]; then
+    LOGR_ROOT_DIR=$MY_DIR/..
 fi
-CDB_ENV_FILE=${CDB_ROOT_DIR}/setup.sh
-if [ ! -f ${CDB_ENV_FILE} ]; then
-    echo "Environment file ${CDB_ENV_FILE} does not exist."
+LOGR_ENV_FILE=${LOGR_ROOT_DIR}/setup.sh
+if [ ! -f ${LOGR_ENV_FILE} ]; then
+    echo "Environment file ${LOGR_ENV_FILE} does not exist."
     exit 2
 fi
-. ${CDB_ENV_FILE} > /dev/null
-CDB_INSTALL_DIR=${CDB_INSTALL_DIR:=$CDB_ROOT_DIR/..}
-CDB_ETC_DIR=${CDB_INSTALL_DIR}/etc
-CDB_LOG_DIR=${CDB_INSTALL_DIR}/var/log
+. ${LOGR_ENV_FILE} > /dev/null
+LOGR_INSTALL_DIR=${LOGR_INSTALL_DIR:=$LOGR_ROOT_DIR/..}
+LOGR_ETC_DIR=${LOGR_INSTALL_DIR}/etc
+LOGR_LOG_DIR=${LOGR_INSTALL_DIR}/var/log
 
-CDB_DB_NAME=cdb
-CDB_WEB_SERVICE_CONFIG_FILE=${CDB_ETC_DIR}/${CDB_DB_NAME}.conf
-CDB_WEB_SERVICE_LOG_FILE=${CDB_LOG_DIR}/${CDB_DB_NAME}.log
-CDB_DB_PASSWORD_FILE=$CDB_INSTALL_DIR/etc/${CDB_DB_NAME}.db.passwd
-read -p "Please enter a url for the LDAP server: " CDB_LDAP_AUTH_SERVER_URL
-read -p "Please enter the dn string for the LDAP server (%s is replaced with username when lookup not needed): " CDB_LDAP_AUTH_DN_FORMAT
-read -p "Please enter the lookup service dn (don't specify if no user lookup required): " CDB_LDAP_SERVICE_DN
-read -p "Please enter the lookup service password (don't specify if no user lookup required): " CDB_LDAP_SERVICE_PASS
-read -p "Please enter the user lookup filter. (%s is replaced with username; don't specify if no user lookup required): " CDB_LDAP_LOOKUP_FILTER
+LOGR_DB_NAME=logr
+LOGR_WEB_SERVICE_CONFIG_FILE=${LOGR_ETC_DIR}/${LOGR_DB_NAME}.conf
+LOGR_WEB_SERVICE_LOG_FILE=${LOGR_LOG_DIR}/${LOGR_DB_NAME}.log
+LOGR_DB_PASSWORD_FILE=$LOGR_INSTALL_DIR/etc/${LOGR_DB_NAME}.db.passwd
+read -p "Please enter a url for the LDAP server: " LOGR_LDAP_AUTH_SERVER_URL
+read -p "Please enter the dn string for the LDAP server (%s is replaced with username when lookup not needed): " LOGR_LDAP_AUTH_DN_FORMAT
+read -p "Please enter the lookup service dn (don't specify if no user lookup required): " LOGR_LDAP_SERVICE_DN
+read -p "Please enter the lookup service password (don't specify if no user lookup required): " LOGR_LDAP_SERVICE_PASS
+read -p "Please enter the user lookup filter. (%s is replaced with username; don't specify if no user lookup required): " LOGR_LDAP_LOOKUP_FILTER
 
-CDB_LDAP_LOOKUP_FILTER=`echo ${CDB_LDAP_LOOKUP_FILTER/&/\\\&}`
+LOGR_LDAP_LOOKUP_FILTER=`echo ${LOGR_LDAP_LOOKUP_FILTER/&/\\\&}`
 
 echo "Preparing development configuration"
-mkdir -p $CDB_ETC_DIR
-mkdir -p $CDB_LOG_DIR
+mkdir -p $LOGR_ETC_DIR
+mkdir -p $LOGR_LOG_DIR
 
 echo "Modifying glassfish-web config file"
-portalSrcDir=$CDB_ROOT_DIR/src/java/CdbWebPortal
+portalSrcDir=$LOGR_ROOT_DIR/src/java/LogrPortal
 configFile=$portalSrcDir/web/WEB-INF/glassfish-web.xml
-cmd="cat $configFile.template | sed 's?CDB_DATA_DIR?$CDB_DATA_DIR?g' > $configFile"
+cmd="cat $configFile.template | sed 's?LOGR_DATA_DIR?$LOGR_DATA_DIR?g' > $configFile"
 eval $cmd || exit 1
 
 echo "Modifying web config file"
 webConfigFile=$portalSrcDir/web/WEB-INF/web.xml
 cmd="cat $webConfigFile.template \
-    | sed 's?CDB_PROJECT_STAGE?Development?g' \
+    | sed 's?LOGR_PROJECT_STAGE?Development?g' \
     > $webConfigFile"
 eval $cmd || exit 1
 
 echo "Modifying cdb.portal.properties config file"
-portalSrcDir=$CDB_ROOT_DIR/src/java/CdbWebPortal
+portalSrcDir=$LOGR_ROOT_DIR/src/java/LogrPortal
 configFile=$portalSrcDir/src/java/cdb.portal.properties
 cmd="cat $configFile.template \
-    | sed 's?CDB_LDAP_AUTH_SERVER_URL?$CDB_LDAP_AUTH_SERVER_URL?g' \
-    | sed 's?CDB_LDAP_AUTH_DN_FORMAT?$CDB_LDAP_AUTH_DN_FORMAT?g' \
-    | sed 's?CDB_LDAP_SERVICE_DN?$CDB_LDAP_SERVICE_DN?g' \
-    | sed 's?CDB_LDAP_SERVICE_PASS?$CDB_LDAP_SERVICE_PASS?g' \
-    | sed 's?CDB_LDAP_LOOKUP_FILTER?$CDB_LDAP_LOOKUP_FILTER?g' \
-    | sed 's?CDB_DATA_DIR?$CDB_DATA_DIR?g' \
+    | sed 's?LOGR_LDAP_AUTH_SERVER_URL?$LOGR_LDAP_AUTH_SERVER_URL?g' \
+    | sed 's?LOGR_LDAP_AUTH_DN_FORMAT?$LOGR_LDAP_AUTH_DN_FORMAT?g' \
+    | sed 's?LOGR_LDAP_SERVICE_DN?$LOGR_LDAP_SERVICE_DN?g' \
+    | sed 's?LOGR_LDAP_SERVICE_PASS?$LOGR_LDAP_SERVICE_PASS?g' \
+    | sed 's?LOGR_LDAP_LOOKUP_FILTER?$LOGR_LDAP_LOOKUP_FILTER?g' \
+    | sed 's?LOGR_DATA_DIR?$LOGR_DATA_DIR?g' \
     > $configFile"
 eval $cmd || exit 1
 
 echo "Configuring glassfish db access"
-if [ ! -f $CDB_DB_PASSWORD_FILE ]; then
-    echo "File $CDB_DB_PASSWORD_FILE does not exist."
+if [ ! -f $LOGR_DB_PASSWORD_FILE ]; then
+    echo "File $LOGR_DB_PASSWORD_FILE does not exist."
     exit 1
 fi
 
-CDB_DB_PASSWORD=`cat $CDB_DB_PASSWORD_FILE`
+LOGR_DB_PASSWORD=`cat $LOGR_DB_PASSWORD_FILE`
 configFile=$portalSrcDir/setup/glassfish-resources.xml
-cmd="cat $configFile.template | sed 's?CDB_DB_PASSWORD?$CDB_DB_PASSWORD?g' > $configFile"
+cmd="cat $configFile.template | sed 's?LOGR_DB_PASSWORD?$LOGR_DB_PASSWORD?g' > $configFile"
 eval $cmd || exit 1
 
-CDB_PORTAL_URL="http://localhost:8080/cdb"
+LOGR_PORTAL_URL="http://localhost:8080/cdb"
 
 read -p "Please enter developer email address, used for web service email notification testing (Optional): " ADMIN_EMAIL_ADDRESS
 
@@ -91,41 +91,41 @@ else
 fi
 
 # uncomment principal authenticator
-if [ ! -z $CDB_LDAP_SERVICE_DN ]; then
+if [ ! -z $LOGR_LDAP_SERVICE_DN ]; then
     uncommentAuthenticator="sed 's?#principalAuthenticator3?principalAuthenticator3?g'"
 else
     uncommentAuthenticator="sed 's?#principalAuthenticator2?principalAuthenticator2?g'"
 fi
 
-CDB_SENDER_EMAIL_ADDRESS='cdb@cdb'
-EMAIL_SUBJECT_START="[CDB_DEV] - `hostname` : "
+LOGR_SENDER_EMAIL_ADDRESS='cdb@cdb'
+EMAIL_SUBJECT_START="[LOGR_DEV] - `hostname` : "
 
-CDB_LDAP_LOOKUP_FILTER=`echo ${CDB_LDAP_LOOKUP_FILTER/&/\\\&}`
+LOGR_LDAP_LOOKUP_FILTER=`echo ${LOGR_LDAP_LOOKUP_FILTER/&/\\\&}`
 
 echo "Generating web service config file"
-cmd="cat $CDB_ROOT_DIR/etc/cdb-web-service.conf.template \
+cmd="cat $LOGR_ROOT_DIR/etc/cdb-web-service.conf.template \
     | $uncommentAuthenticator \
     | sed 's?sslCaCertFile=.*??g' \
     | sed 's?sslCertFile=.*??g' \
     | sed 's?sslKeyFile=.*??g' \
-    | sed 's?CDB_INSTALL_DIR?$CDB_INSTALL_DIR?g' \
-    | sed 's?CDB_DB_NAME?$CDB_DB_NAME?g' \
-    | sed 's?handler=TimedRotatingFileLoggingHandler.*?handler=TimedRotatingFileLoggingHandler(\"$CDB_WEB_SERVICE_LOG_FILE\")?g' \
-    | sed 's?CDB_PORTAL_URL?$CDB_PORTAL_URL?g' \
+    | sed 's?LOGR_INSTALL_DIR?$LOGR_INSTALL_DIR?g' \
+    | sed 's?LOGR_DB_NAME?$LOGR_DB_NAME?g' \
+    | sed 's?handler=TimedRotatingFileLoggingHandler.*?handler=TimedRotatingFileLoggingHandler(\"$LOGR_WEB_SERVICE_LOG_FILE\")?g' \
+    | sed 's?LOGR_PORTAL_URL?$LOGR_PORTAL_URL?g' \
     | sed 's?EMAIL_UTILITY_MODE?$EMAIL_UTILITY_MODE?g' \
-    | sed 's?CDB_SENDER_EMAIL_ADDRESS?$CDB_SENDER_EMAIL_ADDRESS?g' \
+    | sed 's?LOGR_SENDER_EMAIL_ADDRESS?$LOGR_SENDER_EMAIL_ADDRESS?g' \
     | sed 's?ADMIN_EMAIL_ADDRESS?$ADMIN_EMAIL_ADDRESS?g' \
     | sed 's?EMAIL_SUBJECT_START?$EMAIL_SUBJECT_START?g' \
-    | sed 's?CDB_LDAP_AUTH_DN_FORMAT?$CDB_LDAP_AUTH_DN_FORMAT?g' \
-    | sed 's?CDB_LDAP_AUTH_SERVER_URL?$CDB_LDAP_AUTH_SERVER_URL?g' \
-    | sed 's?CDB_LDAP_SERVICE_DN?$CDB_LDAP_SERVICE_DN?g' \
-    | sed 's?CDB_LDAP_SERVICE_PASS?$CDB_LDAP_SERVICE_PASS?g' \
-    | sed 's?CDB_LDAP_LOOKUP_FILTER?$CDB_LDAP_LOOKUP_FILTER?g' \
-    | sed 's?CDB_DATA_DIR?$CDB_DATA_DIR?g'\
-    > $CDB_WEB_SERVICE_CONFIG_FILE"
+    | sed 's?LOGR_LDAP_AUTH_DN_FORMAT?$LOGR_LDAP_AUTH_DN_FORMAT?g' \
+    | sed 's?LOGR_LDAP_AUTH_SERVER_URL?$LOGR_LDAP_AUTH_SERVER_URL?g' \
+    | sed 's?LOGR_LDAP_SERVICE_DN?$LOGR_LDAP_SERVICE_DN?g' \
+    | sed 's?LOGR_LDAP_SERVICE_PASS?$LOGR_LDAP_SERVICE_PASS?g' \
+    | sed 's?LOGR_LDAP_LOOKUP_FILTER?$LOGR_LDAP_LOOKUP_FILTER?g' \
+    | sed 's?LOGR_DATA_DIR?$LOGR_DATA_DIR?g'\
+    > $LOGR_WEB_SERVICE_CONFIG_FILE"
 eval $cmd || exit 1
-#rsync -ar $CDB_DB_PASSWORD_FILE $CDB_ETC_DIR || exit 1
+#rsync -ar $LOGR_DB_PASSWORD_FILE $LOGR_ETC_DIR || exit 1
 
-python $CDB_ROOT_DIR/tools/developer_tools/cdb_plugins/update_plugin_generated_files.py
+python $LOGR_ROOT_DIR/tools/developer_tools/LOGR_plugins/update_plugin_generated_files.py
 
 echo "Done preparing development configuration"
--- setup.sh
+++ setup.sh
@@ -20,57 +20,57 @@ fi
 myDir=`dirname $input_param`
 
 currentDir=`pwd` && cd $myDir
-if [ ! -z "$CDB_ROOT_DIR" -a "$CDB_ROOT_DIR" != `pwd` ]; then
-    echo "WARNING: Resetting CDB_ROOT_DIR environment variable (old value: $CDB_ROOT_DIR)" 
+if [ ! -z "$LOGR_ROOT_DIR" -a "$LOGR_ROOT_DIR" != `pwd` ]; then
+    echo "WARNING: Resetting LOGR_ROOT_DIR environment variable (old value: $LOGR_ROOT_DIR)" 
 fi
-export CDB_ROOT_DIR=`pwd`
+export LOGR_ROOT_DIR=`pwd`
 
-if [ -z $CDB_INSTALL_DIR ]; then
-    export CDB_INSTALL_DIR=$CDB_ROOT_DIR/..
-    if [ -d $CDB_INSTALL_DIR ]; then
-        cd $CDB_INSTALL_DIR
-        export CDB_INSTALL_DIR=`pwd`
+if [ -z $LOGR_INSTALL_DIR ]; then
+    export LOGR_INSTALL_DIR=$LOGR_ROOT_DIR/..
+    if [ -d $LOGR_INSTALL_DIR ]; then
+        cd $LOGR_INSTALL_DIR
+        export LOGR_INSTALL_DIR=`pwd`
     fi
 fi
-if [ -z $CDB_DATA_DIR ]; then
-    export CDB_DATA_DIR=$CDB_INSTALL_DIR/data
-    if [ -d $CDB_DATA_DIR ]; then
-        cd $CDB_DATA_DIR
-        export CDB_DATA_DIR=`pwd`
+if [ -z $LOGR_DATA_DIR ]; then
+    export LOGR_DATA_DIR=$LOGR_INSTALL_DIR/data
+    if [ -d $LOGR_DATA_DIR ]; then
+        cd $LOGR_DATA_DIR
+        export LOGR_DATA_DIR=`pwd`
     fi
 fi
-if [ ! -d $CDB_DATA_DIR ]; then
-    echo "WARNING: $CDB_DATA_DIR directory does not exist. Developers should point CDB_DATA_DIR to the desired area." 
-    #unset CDB_DATA_DIR
+if [ ! -d $LOGR_DATA_DIR ]; then
+    echo "WARNING: $LOGR_DATA_DIR directory does not exist. Developers should point LOGR_DATA_DIR to the desired area." 
+    #unset LOGR_DATA_DIR
 fi
 
-if [ -z $CDB_VAR_DIR ]; then
-    export CDB_VAR_DIR=$CDB_INSTALL_DIR/var
-    if [ -d $CDB_VAR_DIR ]; then
-        cd $CDB_VAR_DIR
-        export CDB_VAR_DIR=`pwd`
+if [ -z $LOGR_VAR_DIR ]; then
+    export LOGR_VAR_DIR=$LOGR_INSTALL_DIR/var
+    if [ -d $LOGR_VAR_DIR ]; then
+        cd $LOGR_VAR_DIR
+        export LOGR_VAR_DIR=`pwd`
     else
-    	unset CDB_VAR_DIR
+    	unset LOGR_VAR_DIR
     fi
 fi
 
 # Establish machine architecture and host name
-CDB_HOST_ARCH=$(uname -sm | tr -s '[:upper:][:blank:]' '[:lower:][\-]')
-CDB_SHORT_HOSTNAME=`hostname -s`
+LOGR_HOST_ARCH=$(uname -sm | tr -s '[:upper:][:blank:]' '[:lower:][\-]')
+LOGR_SHORT_HOSTNAME=`hostname -s`
 
 # Check support setup
-if [ -z $CDB_SUPPORT_DIR ]; then
-    export CDB_SUPPORT_DIR=$CDB_INSTALL_DIR/support-$CDB_SHORT_HOSTNAME
-    if [ -d $CDB_SUPPORT_DIR ]; then
-        cd $CDB_SUPPORT_DIR
-        export CDB_SUPPORT_DIR=`pwd`
+if [ -z $LOGR_SUPPORT_DIR ]; then
+    export LOGR_SUPPORT_DIR=$LOGR_INSTALL_DIR/support-$LOGR_SHORT_HOSTNAME
+    if [ -d $LOGR_SUPPORT_DIR ]; then
+        cd $LOGR_SUPPORT_DIR
+        export LOGR_SUPPORT_DIR=`pwd`
     fi
 fi
-if [ ! -d $CDB_SUPPORT_DIR ]; then
-    echo "Warning: $CDB_SUPPORT_DIR directory does not exist. Developers should point CDB_SUPPORT_DIR to the desired area." 
-    #unset CDB_SUPPORT_DIR
+if [ ! -d $LOGR_SUPPORT_DIR ]; then
+    echo "Warning: $LOGR_SUPPORT_DIR directory does not exist. Developers should point LOGR_SUPPORT_DIR to the desired area." 
+    #unset LOGR_SUPPORT_DIR
 else
-    export CDB_GLASSFISH_DIR=$CDB_SUPPORT_DIR/payara/$CDB_HOST_ARCH
+    export LOGR_GLASSFISH_DIR=$LOGR_SUPPORT_DIR/payara/$LOGR_HOST_ARCH
 fi
 
 # Add to path only if directory exists.
@@ -81,16 +81,16 @@ prependPathIfDirExists() {
     fi
 }
 
-prependPathIfDirExists $CDB_GLASSFISH_DIR/bin
-prependPathIfDirExists $CDB_SUPPORT_DIR/java/$CDB_HOST_ARCH/bin
-prependPathIfDirExists $CDB_SUPPORT_DIR/ant/bin
-prependPathIfDirExists $CDB_SUPPORT_DIR/netbeans/currentNetbeans/bin
-prependPathIfDirExists $CDB_ROOT_DIR/bin
-prependPathIfDirExists $CDB_SUPPORT_DIR/anaconda/$CDB_HOST_ARCH/bin
-prependPathIfDirExists $CDB_SUPPORT_DIR/netbeans/currentNetbeans/java/maven/bin
-prependPathIfDirExists $CDB_ROOT_DIR/tools/developer_tools/portal_testing/PythonSeleniumTest/support_bin
+prependPathIfDirExists $LOGR_GLASSFISH_DIR/bin
+prependPathIfDirExists $LOGR_SUPPORT_DIR/java/$LOGR_HOST_ARCH/bin
+prependPathIfDirExists $LOGR_SUPPORT_DIR/ant/bin
+prependPathIfDirExists $LOGR_SUPPORT_DIR/netbeans/currentNetbeans/bin
+prependPathIfDirExists $LOGR_ROOT_DIR/bin
+prependPathIfDirExists $LOGR_SUPPORT_DIR/anaconda/$LOGR_HOST_ARCH/bin
+prependPathIfDirExists $LOGR_SUPPORT_DIR/netbeans/currentNetbeans/java/maven/bin
+prependPathIfDirExists $LOGR_ROOT_DIR/tools/developer_tools/portal_testing/PythonSeleniumTest/support_bin
 
-mysqlPath=$CDB_SUPPORT_DIR/mysql/$CDB_HOST_ARCH
+mysqlPath=$LOGR_SUPPORT_DIR/mysql/$LOGR_HOST_ARCH
 if [ -d $mysqlPath ]; then
     cd $mysqlPath
     pythonDir=`pwd`
@@ -99,25 +99,25 @@ if [ -d $mysqlPath ]; then
 fi
 
 # Check if we have  local python
-if [ -z $CDB_PYTHON_DIR ]; then
-    pythonDir=$CDB_SUPPORT_DIR/python/$CDB_HOST_ARCH
+if [ -z $LOGR_PYTHON_DIR ]; then
+    pythonDir=$LOGR_SUPPORT_DIR/python/$LOGR_HOST_ARCH
 else
-    pythonDir=$CDB_PYTHON_DIR
+    pythonDir=$LOGR_PYTHON_DIR
 fi
 if [ -d $pythonDir ]; then
     cd $pythonDir
     pythonDir=`pwd`
     export PATH=`pwd`/bin:$PATH
     export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-    export CDB_PYTHON_DIR=$pythonDir
+    export LOGR_PYTHON_DIR=$pythonDir
 fi
 
 if [ -z $PYTHONPATH ]; then
-    PYTHONPATH=$CDB_ROOT_DIR/src/python
+    PYTHONPATH=$LOGR_ROOT_DIR/src/python
 else
-    PYTHONPATH=$CDB_ROOT_DIR/src/python:$PYTHONPATH
+    PYTHONPATH=$LOGR_ROOT_DIR/src/python:$PYTHONPATH
 fi
-PYTHONPATH=$CDB_ROOT_DIR/tools/developer_tools/python-client:$PYTHONPATH
+PYTHONPATH=$LOGR_ROOT_DIR/tools/developer_tools/python-client:$PYTHONPATH
 export PYTHONPATH
 
 # Done
