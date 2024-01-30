322341bb9ff14181588ebc5f74dc13cb8aa9125c
Dariusz Jarosz <djarosz@anl.gov>
2024-01-30 15:14:40 -0600

    Add DB changes for the "default" template per entity type. Closes #60

--- db/sql/updates/updateTo2024.1.sql
+++ db/sql/updates/updateTo2024.1.sql
@@ -1,1 +1,5 @@
 -- Backup and restore db to repopulate the entity-types. 
+
+ALTER TABLE entity_type ADD column `primary_template_item_id` int(11) unsigned NULL AFTER description;
+ALTER TABLE entity_type ADD KEY `primary_template_item_entity_type_k1` (`primary_template_item_id`);
+ALTER TABLE entity_type ADD CONSTRAINT `primary_template_item_entity_type_fk1` FOREIGN KEY (`primary_template_item_id`) REFERENCES `item` (`id`) ON UPDATE CASCADE;
