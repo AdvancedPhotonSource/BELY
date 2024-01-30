2a724972a9c58ca4fa07b3f9480e6f27882353f7
Dariusz Jarosz <djarosz@anl.gov>
2024-01-30 15:24:20 -0600

    Add some instructions to execute update file.

--- db/sql/updates/updateTo2024.1.sql
+++ db/sql/updates/updateTo2024.1.sql
@@ -1,4 +1,6 @@
--- Backup and restore db to repopulate the entity-types. 
+-- Backup and restore db to repopulate the entity-types.
+-- Execute by running `mysql BELY_DB_NAME --host=127.0.0.1 --user=logr -p < updateTo2024.1.sql`
+-- 
 
 ALTER TABLE entity_type ADD column `primary_template_item_id` int(11) unsigned NULL AFTER description;
 ALTER TABLE entity_type ADD KEY `primary_template_item_entity_type_k1` (`primary_template_item_id`);
