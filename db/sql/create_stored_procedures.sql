389214e3d28ecc9fdd601951159b39720abc085a
Dariusz Jarosz <djarosz@anl.gov>
2024-03-26 11:24:31 -0500

    Support multiple logbooks and systems. Resolve filtering logs within children of top level, for example ops. Closes #14

--- db/sql/create_stored_procedures.sql
+++ db/sql/create_stored_procedures.sql
@@ -32,7 +32,7 @@ BEGIN
 END //
 
 DROP PROCEDURE IF EXISTS search_items_no_parent;//
-CREATE PROCEDURE `search_items_no_parent` (IN limit_row int, IN domain_id int, IN entity_type_id INT, IN item_type_id int, IN start_time datetime, IN end_time datetime, IN search_string VARCHAR(255)) 
+CREATE PROCEDURE `search_items_no_parent` (IN limit_row int, IN domain_id int, IN entity_type_id_list TEXT, IN item_type_id_list TEXT, IN start_time datetime, IN end_time datetime, IN search_string VARCHAR(255)) 
 BEGIN
 	SET search_string = CONCAT('"%', search_string, '%"'); 
 
@@ -62,20 +62,18 @@ BEGIN
 	"OR updateu.username LIKE ", search_string, " ",
 	") ");
 
-	IF entity_type_id THEN			
+	IF entity_type_id_list THEN			
 		SET @from_stmt = CONCAT(@from_stmt, "
-			INNER JOIN item_entity_type iet on item.id = iet.item_id
-			INNER JOIN entity_type et on iet.entity_type_id = et.id "); 
+			INNER JOIN item_entity_type iet on item.id = iet.item_id"); 
 		SET @where_stmt = CONCAT(@where_stmt, "
-			AND (iet.entity_type_id = ", entity_type_id, "
-			OR et.parent_entity_type_id = ", entity_type_id, ") ");
+			AND FIND_IN_SET(iet.entity_type_id, '", entity_type_id_list, "') ");			
 	END IF;
 
-	IF item_type_id THEN 
+	IF item_type_id_list THEN 
 		SET @from_stmt = CONCAT(@from_stmt, "
 			INNER JOIN item_item_type iit on item.id = iit.item_id ");
 		set @where_stmt = CONCAT(@where_stmt, "
-			AND iit.item_type_id = ", item_type_id, " ");
+			AND FIND_IN_SET(iit.item_type_id, '", item_type_id_list, "') ");
 	END IF; 
 
 	IF start_time THEN
@@ -105,7 +103,7 @@ BEGIN
 END //
 
 DROP PROCEDURE IF EXISTS search_item_logs;//
-CREATE PROCEDURE `search_item_logs` (IN limit_row int, IN domain_id int, IN entity_type_id INT, IN item_type_id int, IN start_time datetime, IN end_time datetime, IN search_string VARCHAR(255)) 
+CREATE PROCEDURE `search_item_logs` (IN limit_row int, IN domain_id int, IN entity_type_id_list TEXT, IN item_type_id_list TEXT, IN start_time datetime, IN end_time datetime, IN search_string VARCHAR(255)) 
 BEGIN
 	SET search_string = CONCAT('%', search_string, '%'); 
 
@@ -114,7 +112,10 @@ BEGIN
 	INNER JOIN v_item_self_element ise ON item.id = ise.item_id 
 	INNER JOIN item_element ie ON ise.self_element_id = ie.id
 	LEFT OUTER JOIN item_element_log iel on iel.item_element_id = ie.id
-	INNER JOIN log on log.id = iel.log_id "; 
+	INNER JOIN log on log.id = iel.log_id 
+	LEFT OUTER JOIN v_item_hierarchy cih on cih.child_item_id = item.id"; 
+
+	SET @from_tbls = ", item as parent_item "; 
 
 	SET @where_stmt = "WHERE ";
 	SET @where_stmt = CONCAT(@where_stmt, 'item.domain_id = ', domain_id, ' '); 
@@ -127,20 +128,18 @@ BEGIN
 
 	SET @where_stmt = CONCAT(@where_stmt, 'AND (log.text LIKE "', search_string, '") '); 
 
-	IF item_type_id THEN 
-		SET @from_stmt = CONCAT(@from_stmt, "
-			INNER JOIN item_item_type iit on item.id = iit.item_id ");
+	IF item_type_id_list THEN 
+		SET @from_tbls = CONCAT(@from_tbls, ", item_item_type iit");		
 		set @where_stmt = CONCAT(@where_stmt, "
-			AND iit.item_type_id = ", item_type_id, " ");
+			AND parent_item.id = iit.item_id 
+			AND FIND_IN_SET(iit.item_type_id, '", item_type_id_list, "') ");
 	END IF; 
 
-	IF entity_type_id THEN			
-		SET @from_stmt = CONCAT(@from_stmt, "
-			INNER JOIN item_entity_type iet on item.id = iet.item_id
-			INNER JOIN entity_type et on iet.entity_type_id = et.id "); 
+	IF entity_type_id_list THEN			
+		SET @from_tbls = CONCAT(@from_tbls, ", item_entity_type as iet");
 		SET @where_stmt = CONCAT(@where_stmt, "
-			AND (iet.entity_type_id = ", entity_type_id, "
-			OR et.parent_entity_type_id = ", entity_type_id, ") ");
+			AND parent_item.id = iet.item_id 
+			AND FIND_IN_SET(iet.entity_type_id, '", entity_type_id_list, "') ");			
 	END IF;
 	
 	IF start_time THEN
@@ -153,9 +152,7 @@ BEGIN
 			AND log.last_modified_on_date_time < '", end_time, "' ");			
 	END IF;
 
-	SET @from_stmt = CONCAT(@from_stmt, "
-		LEFT OUTER JOIN v_item_hierarchy cih on cih.child_item_id = item.id,
-		item as parent_item "); 
+	SET @from_stmt = CONCAT(@from_stmt, @from_tbls, " "); 
 		
 	SET @sql_stmt = CONCAT(@select_stmt, @from_stmt, @where_stmt, "
 		ORDER BY parent_item.id DESC 
