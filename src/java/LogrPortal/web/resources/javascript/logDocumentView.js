83cec1e017f6a0be327c77836ade9cd8b9b8f009
Dariusz Jarosz <djarosz@anl.gov>
2024-04-03 12:48:01 -0500

    Attach Screenshot or other file from clipboard by pasting into log entry text. Closes #150

--- src/java/LogrPortal/web/resources/javascript/logDocumentView.js
+++ src/java/LogrPortal/web/resources/javascript/logDocumentView.js
@@ -3,6 +3,43 @@
  * See LICENSE file.
  */
 
+document.addEventListener('paste', pasteLogTextArea);
+
+function pasteLogTextArea(event) {
+    pastedFiles = event.clipboardData.files;
+
+    if (pastedFiles.length > 0) {
+        event.preventDefault(); 
+        
+        // Clear Upload Ref
+        uploadRef = document.getElementById('logbookViewForm:lastFileReference'); 
+        uploadRef.innerHTML = ""
+        
+        PF('loadingDialog').show();
+        fileUploadWidget = PF('logEntryClipboardAttachmentFileUploadWidget');
+        uploadInput = document.getElementById('logbookViewForm:logEntryClipboardAttachmentFileUpload'); 
+        uploadInput.files = pastedFiles;
+        fileUploadWidget.upload();        
+    }
+}
+
+function pasteLatestFileReference() {                
+    let uploadRef = document.getElementById('logbookViewForm:lastFileReference'); 
+    let fileRefMd = uploadRef.innerHTML; 
+    
+    if (fileRefMd === "") {
+        console.error("File reference not set"); 
+    }
+    
+    let textArea = document.getElementById('logbookViewForm:logbookLogEntryValue');    
+    let newData = "\n\n" + fileRefMd + "\n\n";
+    let exitingValue = $(textArea).val();
+    let curPos = textArea.selectionStart; 
+    
+    newData=exitingValue.slice(0,curPos)+newData+exitingValue.slice(curPos); 
+    $(textArea).val(newData);        
+}
+
 function ScrollToLog(logId) {
     if (logId) {
         var location = 'log-' + logId;
@@ -25,13 +62,13 @@ function toggleTimestamps() {
 }
 
 $('.logEntry').dblclick(function (event) {
-    console.log("Log Entry Double Clicked"); 
+    console.log("Log Entry Double Clicked");
     var logEntryPanel = event.delegateTarget;
     var logObjectPanel = logEntryPanel.parentElement;
     var logEditButtons = logObjectPanel.getElementsByClassName('logEditButton');
-    
+
     if (logEditButtons.length == 1) {
-        var editButton = logEditButtons[0];        
-        editButton.click();        
+        var editButton = logEditButtons[0];
+        editButton.click();
     }
 });
