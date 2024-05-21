8b13be77e36acdc0a64c8005be84ea50f7553ef7
Dariusz Jarosz <djarosz@anl.gov>
2024-05-21 08:59:30 -0500

    Resolve restoring empty database.

---
+++ db/sql/clean/populate_user_session.sql
@@ -0,0 +1,9 @@
+--
+-- Copyright (c) UChicago Argonne, LLC. All rights reserved.
+-- See LICENSE file.
+--
+
+LOCK TABLES `user_session` WRITE;
+/*!40000 ALTER TABLE `user_session` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_session` ENABLE KEYS */;
+UNLOCK TABLES;
