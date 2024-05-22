67c70c0be65ad79e64ac80024dc01c9f0e03164f
Dariusz Jarosz <djarosz@anl.gov>
2024-05-22 13:31:59 -0500

    Update default reactions

--- db/sql/clean/populate_reaction.sql
+++ db/sql/clean/populate_reaction.sql
@@ -9,6 +9,7 @@ INSERT INTO `reaction` VALUES
 (1,'Like',128077,NULL,1.0),
 (2,'Thinking',129300,NULL,2.0),
 (3,'Great Job!',128175,NULL,3.0),
-(4,'Important',10071,NULL,4.0);
+(4,'Laugh',128516,NULL,4.0);
+(5,"Celebrate",127881,NULL,5.0);
 /*!40000 ALTER TABLE `reaction` ENABLE KEYS */;
 UNLOCK TABLES;
