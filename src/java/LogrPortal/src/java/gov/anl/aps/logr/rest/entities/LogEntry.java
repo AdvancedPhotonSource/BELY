92586004f21aba784a79e4cb7dd6d0875d87cef2
Dariusz Jarosz <djarosz@anl.gov>
2025-02-13 14:35:17 -0600

    Add functionality to optionally add replies and reaction to logEntry response. Closes #226

--- src/java/LogrPortal/src/java/gov/anl/aps/logr/rest/entities/LogEntry.java
+++ src/java/LogrPortal/src/java/gov/anl/aps/logr/rest/entities/LogEntry.java
@@ -7,6 +7,7 @@ package gov.anl.aps.logr.rest.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import gov.anl.aps.logr.portal.model.db.entities.ItemDomainLogbook;
 import gov.anl.aps.logr.portal.model.db.entities.Log;
+import gov.anl.aps.logr.portal.model.db.entities.LogReaction;
 import gov.anl.aps.logr.portal.model.db.entities.UserInfo;
 import java.util.ArrayList;
 import java.util.Date;
@@ -30,10 +31,13 @@ public class LogEntry {
     private Date lastModifiedOnDateTime;
     private UserInfo lastModifiedByUser;
 
+    private List<LogEntry> logReplies;
+    private List<LogReaction> logReactions;
+
     public LogEntry() {
     }
 
-    public LogEntry(int itemId, Log log) {
+    public LogEntry(int itemId, Log log, boolean loadReplies, boolean loadReactions) {
         this.itemId = itemId;
         logId = log.getId();
         logEntry = log.getText();
@@ -46,6 +50,14 @@ public class LogEntry {
 
         if (logEntry == null) {
             logEntry = "";
+        }
+
+        if (loadReplies) {
+            logReplies = createLogEntryList(itemId, log.getChildLogList(), false, loadReactions);
+        }
+
+        if (loadReactions) {
+            logReactions = log.getLogReactionList();
         }
     }
 
@@ -79,17 +91,29 @@ public class LogEntry {
         return lastModifiedByUser;
     }
 
+    public List<LogEntry> getLogReplies() {
+        return logReplies;
+    }
+
+    public List<LogReaction> getLogReactions() {
+        return logReactions;
+    }
+
     public void updateLogPerLogEntryObject(Log log) {
         log.setText(logEntry);
     }
 
-    public static List<LogEntry> createLogEntryList(ItemDomainLogbook item) {
-        List<LogEntry> logEntries = new ArrayList<>();
-
+    public static List<LogEntry> createLogEntryList(ItemDomainLogbook item, boolean loadReplies, boolean loadReactions) {
         List<Log> logList = item.getLogList();
 
+        return createLogEntryList(item.getId(), logList, loadReplies, loadReactions);
+    }
+
+    private static List<LogEntry> createLogEntryList(Integer itemId, List<Log> logList, boolean loadReplies, boolean loadReactions) {
+        List<LogEntry> logEntries = new ArrayList<>();
+
         for (Log log : logList) {
-            LogEntry entry = new LogEntry(item.getId(), log);
+            LogEntry entry = new LogEntry(itemId, log, loadReplies, loadReactions);
             logEntries.add(entry);
         }
 
