4acfb0770d4d93378840a16f9d350feec6728277
Dariusz Jarosz <djarosz@anl.gov>
2025-11-17 11:20:59 -0600

    Add details for the log entry and log reply events.
    #2

--- src/java/LogrPortal/src/java/gov/anl/aps/logr/common/mqtt/model/LogEntryEvent.java
+++ src/java/LogrPortal/src/java/gov/anl/aps/logr/common/mqtt/model/LogEntryEvent.java
@@ -4,22 +4,123 @@
  */
 package gov.anl.aps.logr.common.mqtt.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import gov.anl.aps.logr.common.mqtt.constants.MqttTopic;
-import gov.anl.aps.logr.portal.model.db.entities.CdbEntity;
+import gov.anl.aps.logr.portal.model.db.entities.EntityInfo;
+import gov.anl.aps.logr.portal.model.db.entities.ItemDomainLogbook;
+import gov.anl.aps.logr.portal.model.db.entities.Log;
+import java.util.Date;
 
 /**
  *
  * @author djarosz
  */
-public class LogEntryEvent extends MqttEvent {
+public class LogEntryEvent extends MqttEvent<Log> {
 
-    public LogEntryEvent(CdbEntity entity, String description) {
+    LogbookInfo parentLogbookInfo;
+    LogInfo logInfo;
+
+    public LogEntryEvent(ItemDomainLogbook parentLogbook, Log entity, String description) {
         super(entity, description);
+        this.logInfo = new LogInfo(entity);
+
+        if (parentLogbook != null) {
+            parentLogbookInfo = new LogbookInfo(parentLogbook);
+        }
     }
 
     @Override
     public MqttTopic getTopic() {
         return MqttTopic.LOGENTRY;
+    }
+
+    public LogInfo getLogInfo() {
+        return logInfo;
+    }
+
+    public LogbookInfo getParentLogbookInfo() {
+        return parentLogbookInfo;
+    }
+
+    protected class LogInfo {
+
+        Log log;
+
+        public LogInfo(Log log) {
+            this.log = log;
+        }
+
+        public Integer getId() {
+            return log.getId();
+        }
+
+        public String getEnteredByUsername() {
+            return log.getEnteredByUsername();
+        }
+
+        public String getLastModifiedByUsername() {
+            return log.getLastModifiedByUsername();
+        }
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
+        public Date getEnteredOnDateTime() {
+            return log.getEnteredOnDateTime();
+        }
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
+        public Date getLastModifiedOnDateTime() {
+            return log.getLastModifiedOnDateTime();
+        }
+    }
+
+    private class LogbookInfo {
+
+        ItemDomainLogbook parentLogbook;
+
+        public LogbookInfo(ItemDomainLogbook parentLogbook) {
+            this.parentLogbook = parentLogbook;
+        }
+
+        public Integer getId() {
+            return parentLogbook.getId();
+        }
+
+        public String getName() {
+            return parentLogbook.getName();
+        }
+
+        @JsonIgnore
+        public EntityInfo getEntityInfo() {
+            return parentLogbook.getEntityInfo();
+        }
+
+        public String getCreatedByUsername() {
+            return getEntityInfo().getCreatedByUsername();
+        }
+
+        public String getLastModifiedByUsername() {
+            return getEntityInfo().getLastModifiedByUsername();
+        }
+
+        public String getOwnerUsername() {
+            return getEntityInfo().getOwnerUsername();
+        }
+
+        public String getOwnerUserGroupName() {
+            return getEntityInfo().getOwnerUserGroupName();
+        }
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
+        public Date getEnteredOnDateTime() {
+            return getEntityInfo().getCreatedOnDateTime();
+        }
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
+        public Date getLastModifiedOnDateTime() {
+            return getEntityInfo().getLastModifiedOnDateTime();
+        }
+
     }
 
 }
---
+++ src/java/LogrPortal/src/java/gov/anl/aps/logr/common/mqtt/model/ReplyLogEntryEvent.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) UChicago Argonne, LLC. All rights reserved.
+ * See LICENSE file.
+ */
+package gov.anl.aps.logr.common.mqtt.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import gov.anl.aps.logr.common.mqtt.constants.MqttTopic;
+import gov.anl.aps.logr.portal.model.db.entities.ItemDomainLogbook;
+import gov.anl.aps.logr.portal.model.db.entities.Log;
+
+/**
+ *
+ * @author djarosz
+ */
+public class ReplyLogEntryEvent extends LogEntryEvent {
+
+    LogInfo parentlogInfo;
+
+    public ReplyLogEntryEvent(ItemDomainLogbook parentLogbook, Log entity, String description) {
+        super(parentLogbook, entity, description);
+
+        Log parentLogObject = getParentLogObject();
+
+        if (parentLogObject != null) {
+            parentlogInfo = new LogInfo(parentLogObject);
+        }
+    }
+
+    @Override
+    public MqttTopic getTopic() {
+        return MqttTopic.LOGENTRYREPLY;
+    }
+
+    @JsonIgnore
+    public Log getParentLogObject() {
+        if (entity == null) {
+            return null;
+        }
+        return entity.getParentLog();
+    }
+
+    public LogInfo getParentlogInfo() {
+        return parentlogInfo;
+    }
+
+}
