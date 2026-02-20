8f725f413b74d371a905095cef709651daa30cd7
Dariusz Jarosz <djarosz@anl.gov>
2026-02-20 15:06:15 -0600

    Update the api publish script for publishing BELY api to pypi.
    #2

--- tools/developer_tools/python-client/setup-api.py
+++ tools/developer_tools/python-client/setup-api.py
@@ -7,8 +7,8 @@ See LICENSE file.
 
 from setuptools import setup
 
-setup(name='BELY-API',
-      version='2024.6',
+setup(name='bely_api',
+      version='2025.3.dev0',
       packages=["belyApi",
                 "belyApi.api",
                 "belyApi.models"],
