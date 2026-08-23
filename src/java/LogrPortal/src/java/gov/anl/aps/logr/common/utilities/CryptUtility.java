de249edbff17678adbc38dbd25e69e19e137bc81
wmliu <wmliu@anl.gov>
2026-08-22 20:28:26 -0500

    Modified CryptUtility.java to enable local stored password backward compability

--- src/java/LogrPortal/src/java/gov/anl/aps/logr/common/utilities/CryptUtility.java
+++ src/java/LogrPortal/src/java/gov/anl/aps/logr/common/utilities/CryptUtility.java
@@ -26,7 +26,7 @@ public class CryptUtility{
     private static final char[] SaltCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
     private static final String SaltDelimiter = "$";
 
-//    private static final Logger logger = LogManager.getLogger(CryptUtility.class.getName());
+    private static final Logger logger = LogManager.getLogger(CryptUtility.class.getName());
     
     private static final SecureRandom secureRandom = new SecureRandom(); 
     private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder(); 
@@ -92,7 +92,7 @@ public class CryptUtility{
             return SaltDelimiter+"pbkdf2-sha512"+SaltDelimiter+"25000"+SaltDelimiter+encodedSalt + SaltDelimiter + encodedPassword;
         } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
             // Should not happen
-//            logger.error("Password cannot be crypted: " + ex);
+            logger.error("Password cannot be crypted: " + ex);
         }
         return null;
     }
@@ -107,6 +107,7 @@ public class CryptUtility{
     public static String OnlyCryptPasswordWithPbkdf2(String password, String salt) {
         char[] passwordChars = password.toCharArray();
         byte[] saltBytes = (Base64.getDecoder().decode(salt));
+//        byte[] saltBytes = salt.getBytes();
 //		logger.error("calling OnlyCryp...: " + password + "."+salt);
         PBEKeySpec spec = new PBEKeySpec(
                 passwordChars,
@@ -122,7 +123,35 @@ public class CryptUtility{
             return encodedPassword;
         } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
             // Should not happen
-//            logger.error("Password cannot be crypted: " + ex);
+            logger.error("Password cannot be crypted: " + ex);
+        }
+        return null;
+    }
+
+    public static String OnlyCryptPasswordWithSHA1(String password, String salt) {
+        char[] passwordChars = password.toCharArray();
+        byte[] saltBytes = salt.getBytes();
+        String SecretKeyFactoryType1 = "PBKDF2WithHmacSHA1";
+        int Pbkdf2Iterations1 = 1003;
+        int Pbkdf2KeyLengthInBits1 = 192;
+        int SaltLengthInBytes1 = 4;
+//		logger.error("calling OnlyCryp..SHA1.: " + password + "."+salt);
+
+        PBEKeySpec spec = new PBEKeySpec(
+                passwordChars,
+                saltBytes,
+                Pbkdf2Iterations1,
+                Pbkdf2KeyLengthInBits1
+        );
+        SecretKeyFactory key;
+        try {
+            key = SecretKeyFactory.getInstance(SecretKeyFactoryType1);
+            byte[] hashedPassword = key.generateSecret(spec).getEncoded();
+            String encodedPassword = Base64.getEncoder().encodeToString(hashedPassword);
+            return encodedPassword;
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            // Should not happen
+            logger.error("Password cannot be crypted: " + ex);
         }
         return null;
     }
@@ -137,17 +166,31 @@ public class CryptUtility{
      */
     public static boolean verifyPasswordWithPbkdf2(String password, String cryptedPassword) {
         int saltBegin, saltEnd,i;
+// added to make it compatible with old stored password
+	saltBegin=cryptedPassword.indexOf(SaltDelimiter);
+	String salt, pw, epw;
+//        logger.error("calling verify..:"+saltBegin );
+	if( saltBegin!=0 ) //Old password
+	{
+		salt=cryptedPassword.substring(0,saltBegin);
+		pw=cryptedPassword.substring(saltBegin+1);
+		epw=OnlyCryptPasswordWithSHA1(password,salt);
+//        logger.error("salt1:"+salt + " hash$" + pw + "new: " + epw );
+	}
+//
+	else {
         saltBegin=1;
         for (i=0;i<2;i++)
              saltBegin = cryptedPassword.indexOf(SaltDelimiter,saltBegin) + 1;
         saltEnd = cryptedPassword.indexOf(SaltDelimiter,saltBegin);
         
-//        logger.error("password:"+cryptedPassword);
-        String salt = cryptedPassword.substring(saltBegin, saltEnd);
-        String pw=cryptedPassword.substring(saltEnd+1);
+//        logger.error("password:"+cryptedPassword + " begin/end:" + saltBegin + "/ " +saltEnd);
+        salt = cryptedPassword.substring(saltBegin, saltEnd);
+        pw=cryptedPassword.substring(saltEnd+1);
 //        logger.error("saltend:"+,saltEnd+ "T",saltBegin + pw)
-        String epw=OnlyCryptPasswordWithPbkdf2(password, salt);
-//        logger.error("salt:"+salt + " hash$" + pw + "new: " + epw );
+        epw=OnlyCryptPasswordWithPbkdf2(password, salt);
+//        logger.error("salt2:"+salt + " hash$" + pw + "new: " + epw );
+	}
         return pw.equals(epw);
     }
 
