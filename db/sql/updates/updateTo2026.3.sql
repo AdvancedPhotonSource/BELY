6a94a7fb92801556227778a80cd3112528e550ee
Dariusz Jarosz <djarosz@anl.gov>
2026-09-09 09:45:47 -0500

    Update the schema to add a default user group per logbook.

--- db/sql/updates/updateTo2026.3.sql
+++ db/sql/updates/updateTo2026.3.sql
@@ -389,3 +389,11 @@ BEGIN
 END //
 
 delimiter ;
+
+--
+-- Default owner user group per logbook type (entity_type)
+--
+
+ALTER TABLE entity_type ADD column `default_owner_user_group_id` int(11) unsigned DEFAULT NULL AFTER primary_template_item_id;
+ALTER TABLE entity_type ADD KEY `default_owner_user_group_entity_type_k3` (`default_owner_user_group_id`);
+ALTER TABLE entity_type ADD CONSTRAINT `default_owner_user_group_entity_type_fk3` FOREIGN KEY (`default_owner_user_group_id`) REFERENCES `user_group` (`id`) ON UPDATE CASCADE ON DELETE SET NULL;
